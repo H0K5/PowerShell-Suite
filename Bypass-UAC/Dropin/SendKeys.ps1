a0cb755eef3c20e3280ffffc36803619c5a6ca8d
Az <z0rk@protonmail.com>
2017-12-04 12:30:54 +0100

    Update SendKeys.ps1

--- Bypass-UAC/Dropin/SendKeys.ps1
+++ Bypass-UAC/Dropin/SendKeys.ps1
@@ -19,3 +19,23 @@ function kill-window($name){
 	Add-Type -AssemblyName System.Windows.Forms
 	[System.Windows.Forms.SendKeys]::SendWait('%{F4}');
 }
+
+function Show-Proc($Process, $Mode)
+{
+	  $sig = '
+	    [DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+	    [DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);
+	    [DllImport("user32.dll")] public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+	  '	  
+	  $type = Add-Type -MemberDefinition $sig -Name WindowAPI -PassThru
+	  $hwnd = $process.MainWindowHandle
+	  $null = $type::ShowWindowAsync($hwnd, $Mode)
+	  $null = $type::SetForegroundWindow($hwnd) 
+}
+
+### Mode 
+# Hide 0
+# Minimiz 6
+# Show 5
+# Show normal 1
+# Show maximizd 3
